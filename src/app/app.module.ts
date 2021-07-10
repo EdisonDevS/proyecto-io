be1f3a1ad57122270f382e15c98e085e47b313f7
Jhon Edison Giraldo Mejia <jhonedisongiraldo99@gmail.com>
2021-07-09 22:52:42 -0500

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomePage } from './home/home.page';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,11 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as yasmij from 'yasmij';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  funcionObjetivo: string;
+
+  restriccion: string;
+  restricciones: string[] = [];
+
+  min: string;
+  max: string;
+
+  resultado: any;
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+  }
+
+  agregarRestriccion() {
+    this.restricciones.push(this.restriccion);
+  }
+
+  calcular() {
+    const input = {
+      type: 'minimize',
+      objective : this.funcionObjetivo,
+      constraints : this.restricciones
+    };
+
+    this.resultado = yasmij.solve( input );
+    console.log(this.resultado);
+  }
 
 }
